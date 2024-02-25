67ad0b21c05a03cb9381d27ac06232b636092ea0
Yana Mikryukova <yamikryukova@yandex.ru>
2024-02-25 08:40:54 +0300

    Added files

---
+++ main.py
@@ -0,0 +1,11 @@
+import datetime
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.datetime.now().strftime('%Y-%m-%d'))
+
